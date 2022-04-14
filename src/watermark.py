822055a4cb0ec3fa28a5a60354a12c19888ed3b4
Cain Silvey <cains@lcslo.local>
2022-04-14 08:51:29 -0700

    removed re-size function, added user input for photo quality

--- src/watermark.py
+++ src/watermark.py
@@ -4,10 +4,11 @@ from tkinter import *
 from tkinter import filedialog
 import os
 import glob
+# import timer
 
-#Set default to no resize
-global resize_yn
-resize_yn = False
+# #Set default to no resize
+# global resize_yn
+# resize_yn = False
 
 #----------------
 # Define button commands
@@ -17,68 +18,72 @@ resize_yn = False
 # Select source folder
 def sourceFolder():
   global sourcePath
-  sourcePath = filedialog.askdirectory(initialdir="S:\\Active files\\PHOTOS")
+  sourcePath = filedialog.askdirectory(initialdir="S:\\Active files\\PHOTOS\\ResizeTester\\Photo Comparison")
   inputPath.insert(0, sourcePath)
 
 #Select save folder
 def destFolder():
   global destPath 
-  destPath = filedialog.askdirectory(initialdir="S:\\Active files\\PHOTOS")
+  destPath = filedialog.askdirectory(initialdir="S:\\Active files\\PHOTOS\\ResizeTester\\Photo Comparison")
   outputPath.insert(0, destPath)
 
-#Called when resize radio button set to "Yes"
-def resize_yes():
-  global resize_yn
-  resize_yn = True
+# #Called when resize radio button set to "Yes"
+# def resize_yes():
+#   global resize_yn
+#   resize_yn = True
   
-#Called when resize radio button set to "No"
-def resize_no():
-  global resize_yn
-  resize_yn = False 
+# #Called when resize radio button set to "No"
+# def resize_no():
+#   global resize_yn
+#   resize_yn = False 
     
 #Run program
 def watermark():
   global sourcePath
   global destPath
-  global resize_yn
+  # global resize_yn
   path_in = sourcePath + '\\*.jpg'
   text = textEntry.get()
-  if resize_yn is True:
-    font = ImageFont.truetype('arial.ttf', 36)
-    for im in glob.glob(path_in):
-      im_original = PIL.Image.open(im)
-      im_resize = im_original.resize((960,640))
-      width, height = im_resize.size
-      mark = ImageDraw.Draw(im_resize)
-      textwidth, textheight = mark.textsize(text, font)
-      margin_right = width * .15
-      margin_bottom = height * .15
-      x = width - textwidth - margin_right
-      y = height - textheight - margin_bottom
-      mark.text((x,y), text, font=font)
-      im_index = im.rfind('\\')
-      im_name = im[(im_index + 1):]
-      path_out = destPath + '\\' + im_name
-      im_resize.save(path_out)
-    root.destroy()
-  else:
-    for im in glob.glob(path_in):
-      im_original = PIL.Image.open(im)
-      width, height = im_original.size
-      mark = ImageDraw.Draw(im_original)
-      fontsize =int(width * .04)
-      font = ImageFont.truetype('arial.ttf', fontsize)
-      textwidth, textheight = mark.textsize(text, font)
-      margin_right = width * .15
-      margin_bottom = height * .15
-      x = width - textwidth - margin_right
-      y = height - textheight - margin_bottom
-      mark.text((x,y), text, font=font)
-      im_index = im.rfind('\\')
-      im_name = im[(im_index + 1):]
-      path_out = destPath + '\\' + im_name
-      im_original.save(path_out)
-    root.destroy()
+  qual = int(photoQual.get())
+  
+  ### Adds option to resize photo. Keeping this code in case it needs to be reused later. Will need to un-comment
+  # resize radio button functions above, and GUI code down below as well. ###
+  # if resize_yn is True:
+  #   font = ImageFont.truetype('arial.ttf', 36)
+  #   for im in glob.glob(path_in):
+  #     im_original = PIL.Image.open(im)
+  #     im_resize = im_original.resize((960,640))
+  #     width, height = im_resize.size
+  #     mark = ImageDraw.Draw(im_resize)
+  #     textwidth, textheight = mark.textsize(text, font)
+  #     margin_right = width * .15
+  #     margin_bottom = height * .15
+  #     x = width - textwidth - margin_right
+  #     y = height - textheight - margin_bottom
+  #     mark.text((x,y), text, font=font)
+  #     im_index = im.rfind('\\')
+  #     im_name = im[(im_index + 1):]
+  #     path_out = destPath + '\\' + im_name
+  #     im_resize.save(path_out)
+  #   root.destroy()
+  # else:
+  for im in glob.glob(path_in):
+    im_original = PIL.Image.open(im)
+    width, height = im_original.size
+    mark = ImageDraw.Draw(im_original)
+    fontsize =int(width * .04)
+    font = ImageFont.truetype('arial.ttf', fontsize)
+    textwidth, textheight = mark.textsize(text, font)
+    margin_right = width * .15
+    margin_bottom = height * .15
+    x = width - textwidth - margin_right
+    y = height - textheight - margin_bottom
+    mark.text((x,y), text, font=font)
+    im_index = im.rfind('\\')
+    im_name = im[(im_index + 1):]
+    path_out = destPath + '\\' + im_name
+    im_original.save(path_out, quality=qual)
+  root.destroy()
 
     
 
@@ -160,19 +165,32 @@ textLabel.grid(row=4, column=0, padx=10, sticky=W)
 textEntry = Entry(root, width=105)
 textEntry.grid(row=5, column=0, padx=10, pady=20, columnspan=1, sticky=W)
 
-#create radio button label
-radioLabel = Label(root, text="Resize for PowerPoint (960x640)?")
-radioLabel.grid(row=6, column=0, padx=10, sticky=W)
+# create photo quality label
+qualityLabel1 = Label(root, text='Output Photo Quality:')
+qualityLabel1.grid(row=6, column=0, padx=10, sticky=W)
 
-#create radio buttons
-radio_yes = Radiobutton(root, text="Yes", variable=radioSelect, value="Yes", command=resize_yes)
-radio_yes.grid(row=7, column=0, sticky=W)
-radio_no = Radiobutton(root, text="No", variable=radioSelect, value="No", command=resize_no)
-radio_no.grid(row=8, column=0, sticky=W)
+# create photo quality label % sign
+qualityLabel2 = Label(root, text='%')
+qualityLabel2.grid(row=6, column=0, padx=160, sticky=W)
+
+# create photo quality entry box
+photoQual = Entry(root, width=3)
+photoQual.insert(END, '70')
+photoQual.grid(row=6, column=0, padx=135, pady=20, columnspan=1, sticky=W)
+
+# #create radio button label
+# radioLabel = Label(root, text="Resize for PowerPoint (960x640)?")
+# radioLabel.grid(row=6, column=0, padx=10, sticky=W)
+
+# #create radio buttons
+# radio_yes = Radiobutton(root, text="Yes", variable=radioSelect, value="Yes", command=resize_yes)
+# radio_yes.grid(row=7, column=0, sticky=W)
+# radio_no = Radiobutton(root, text="No", variable=radioSelect, value="No", command=resize_no)
+# radio_no.grid(row=8, column=0, sticky=W)
 
 # create save button
 saveButton = Button(root, text="Run", width=10, command=watermark)
-saveButton.grid(row=8, column=2, padx=10, pady=10)
+saveButton.grid(row=6, column=2, padx=10, pady=10)
 
 # run mainloop
 root.mainloop()
